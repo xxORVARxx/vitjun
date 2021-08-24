185ec18af4093453c0226e08804b74d58ee15d9e
xxORVARxx <xxORVARxx@gmail.com>
2021-08-24 07:13:05 +0000

    Add project file.

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+"use strict" // WHOLE-SCRIPT STRICT MODE SYNTAX.
+
+// https://materializecss.com/scrollspy.html:
+document.addEventListener("DOMContentLoaded", () => {
+    f_scrollspy();
+    f_sidenav();
+    f_collapsible();
+});
+
+
+
+function f_scrollspy(){
+    const options = {
+        throttle: 200,
+        scrollOffset: 0,
+    };
+    const elems = document.querySelectorAll(".scrollspy");
+    const instances = M.ScrollSpy.init(elems, options);
+}
+
+function f_sidenav(){
+    const options = {
+        edge: "right",
+        preventScrolling: false,
+    };
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, options);
+}
+
+function f_collapsible(){
+    const options = {
+        accordion: false,
+    };
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+};
+  
+
